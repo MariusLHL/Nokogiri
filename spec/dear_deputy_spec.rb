c86782f145c0d4e46deb14a6bcc26020e17e5b03
mariuslhl <mariuslhl@gmail.com>
2020-01-23 22:30:04 +0100

    ee

--- spec/dear_deputy_spec.rb
+++ spec/dear_deputy_spec.rb
@@ -4,10 +4,8 @@ describe deputy do
   it "should be equal to 577" do
     expect(deputy.size).to eq(577)
   end
-  it "should be an array" do
+  it "should be an array and an hash" do
     expect(deputy).to be_an_instance_of(Array)
-  end
-  it "should be an hash" do
     expect(deputy[0]).to be_an_instance_of(Hash)
   end
   it "shouldn't include nil or empty values" do
--- spec/townhall_spec.rb
+++ spec/townhall_spec.rb
@@ -4,10 +4,10 @@ describe townhall do
   it "should be equal to 200" do
     expect(townhall.size).to eq(185)
   end
-  it "should be equal to 200" do
+  it "should be an array" do
     expect(townhall).to be_an_instance_of(Array)
   end
-  it "should be equal to 200" do
+  it "should be an hash" do
     expect(townhall[0]).to be_an_instance_of(Hash)
   end
 end
